f40671114a2e18a78691ba6c5dcd458db3f73d96
szykor18 <szymonkornik.bs@gmail.com>
2024-03-07 22:23:38 +0100

    added cors config

---
+++ src/main/java/pl/lotto/infrastructure/cors/CorsConfig.java
@@ -0,0 +1,19 @@
+package pl.lotto.infrastructure.cors;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
